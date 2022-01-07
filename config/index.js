6072a02795b00d790b591ec6d9ad61a4d4051b4f
Benjamin Michaelis <benjamin@michaelis.net>
2022-01-07 15:38:33 -0800

    Initialize Site

---
+++ config/index.js
@@ -0,0 +1,23 @@
+module.exports = {
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
+
+  siteTitle: 'Benjamin Michaelis Productions', // Navigation and Site Title
+  siteTitleAlt: 'Benjamin Michaelis Photography Portfolio', // Alternative Site title for SEO
+  siteTitleShort: 'Benjamin Michaelis Photography', // short_name for manifest
+  siteHeadline: 'Benjamin Michaelis Photography: Enjoy some photography', // Headline for schema.org JSONLD
+  siteUrl: 'https://photography.michaelis.net', // Domain of your site. No trailing slash!
+  siteLanguage: 'en', // Language Tag on <html> element
+  siteLogo: '/static/logos/favicon.png', // Used for SEO and manifest
+  siteDescription: `Benjamin Michaelis's Photograph Portfolio`,
+  author: 'BenjaminMichaelis', // Author for schema.org JSONLD
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: '@jodie', // Twitter Username
+  ogSiteName: 'jodie', // Facebook Site Name
+  ogLanguage: 'en_US', // og:language
+  googleAnalyticsID: 'UA-XXXXXX-X',
+
+  // Manifest and Progress color
+  themeColor: '#db7436',
+  backgroundColor: '#3b3c4f',
+}
